05b522862e2d79e8b3ed5515c5c14cba5c5dc7c4
baxtiyorovbekzod <bekzodbaxtiyorov142@gmail.com>
2025-10-19 15:16:48 +0500

    ok

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+from models import Product, Customer, Order, Shop
 
+
+shop = Shop("TechStore")
+
+
+laptop = Product("Laptop", 1200, 5)
+mouse = Product("Mouse", 25, 30)
+shop.add_product(laptop)
+shop.add_product(mouse)
+
+
+user = Customer("Ali", 2000)
+shop.add_customer(user)
+
+
+order = Order(user)
+order.add_item(laptop, 1)
+order.add_item(mouse, 2)
+
+
+print("Total:", order.calculate_total())
+
+
+order.complete_order()
